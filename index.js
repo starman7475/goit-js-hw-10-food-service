fd314350c6a80314075b75e27c6a5d0b99e0cbc2
starman7475 <sirota_mihail2007@mail.ru>
2024-03-25 17:05:02 +0700

    подправил js, с помощью handlebars

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import menuList from "./menu.json";
-import Handlebars from "handlebars";
+import menuTemplate from "./templates/menulist.hbs";
+
+// import Handlebars from "handlebars";
 
 const Theme = {
   LIGHT: "light-theme",
@@ -26,43 +28,6 @@ if (localStorage.getItem("theme") === "dark") {
 const menuListMarkUp = makeMarkUp(menuList);
 refs.placeForMarkUp.insertAdjacentHTML("beforeend", menuListMarkUp);
 
-function makeMarkUp(obj) {
-  return obj
-    .map(() => {
-      return `<li class="menu__item">
-        <article class="card">
-          <img
-            src= {{image}}
-            alt="name"
-            class="card__image"
-          />
-          <div class="card__content">
-            <h2 class="card__name">{{name}}</h2>
-            <p class="card__price">
-              <i class="material-icons"> monetization_on </i>
-              {{price}}
-            </p>
-      
-            <p class="card__descr">
-              {{description}}
-            </p>
-      
-            <ul class="tag-list">
-              <li class="tag-list__item">{{ingredients}}</li>
-              <li class="tag-list__item">{{ingredients}}</li>
-              <li class="tag-list__item">{{ingredients}}</li>
-              <li class="tag-list__item">{{ingredients}}</li>
-              <li class="tag-list__item">{{ingredients}}</li>
-              <li class="tag-list__item">{{ingredients}}</li>
-            </ul>
-          </div>
-      
-          <button class="card__button button">
-            <i class="material-icons button__icon"> shopping_cart </i>
-            В корзину
-          </button>
-        </article>
-      </li>`;
-    })
-    .join("");
+function makeMarkUp(objs) {
+  return objs.map(menuTemplate).join("");
 }
